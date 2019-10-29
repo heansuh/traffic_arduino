acfada213677d980e03d9a5acd2e0496f50eedf6
Heansuh <49907531+heansuh@users.noreply.github.com>
2019-10-29 18:49:57 +0900

    Update code.c
    - If-else statements are replaced with switch-case statements
    - Newly updated circuit board: https://www.tinkercad.com/things/5HjTecqKy72-microcontroller-practical-task-pena-moaz-heansuh/editel
    - .brd file is attached (micro_pr2_update.brd)

--- code.c
+++ code.c
@@ -1,9 +1,11 @@
-int red = 13;
-int yellow = 12;
-int green = 8;
-int buttn1 = 2;
+const int red = 3;
+const int yellow = 4;
+const int green = 5;
+const int buttn1 = 7;
+
 //int buttn2 = 7;
 
+int currentState = red;
 
 void setup() {
 
@@ -20,7 +22,73 @@ void setup() {
 // the loop function runs over and over again forever
 
 void loop() {
-
+  
+  switch(currentState)
+  {
+    
+    case red:
+    
+    // not sure if below one is needed, same result at the end
+    
+    /*
+    
+    if (digitalRead(buttn1) == HIGH)
+    {
+      currentState = yellow;
+    }
+    
+    */
+    
+    digitalWrite(red, HIGH);
+    delay(3000);
+    digitalWrite(red, LOW);
+    delay(200);
+    currentState = yellow;
+    
+    case yellow:
+    
+    if (digitalRead(buttn1) == HIGH)
+    {
+      digitalWrite(yellow, HIGH);
+      delay(5000);
+      digitalWrite(yellow, LOW);
+      delay(200);
+      currentState = green;
+    }
+    
+    else
+    {
+      digitalWrite(yellow, HIGH);
+      delay(3000);
+      digitalWrite(yellow, LOW);
+      delay(100);
+      currentState = green;
+    }
+    
+    case green:
+    
+    if (digitalRead(buttn1) == HIGH)
+    {
+      digitalWrite(green, HIGH);
+      delay(9000);
+      digitalWrite(green, LOW);
+      delay(200);
+      currentState = yellow;
+    }
+    
+    else
+    {
+      digitalWrite(green, HIGH);
+      delay(3000);
+      digitalWrite(green, LOW);
+      delay(200);
+      currentState = red;
+    }
+    
+  }
+}
+    
+  /*
   
   if (digitalRead(buttn1) == HIGH) {
   
@@ -36,8 +104,6 @@ void loop() {
   delay(200);                               // wait for a second
   
   }
-  
-
   
   else 
   digitalWrite(red, HIGH);                    // turn the LED on (HIGH is the voltage level)
@@ -63,5 +129,5 @@ void loop() {
   delay(2000);                              // wait for a second
   digitalWrite(yellow, LOW);                // turn the LED off by making the voltage LOW
   delay(100);                               // wait for a second
- 
-}
+  
+} */
